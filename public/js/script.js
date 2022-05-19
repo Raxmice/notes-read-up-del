955f53e7dbdba94eb1ab267f7a8f73e34b11ae29
Raxmice <93811296+Raxmice@users.noreply.github.com>
2022-05-19 14:51:48 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
 
     function del(){
-    let tf = confirm("Are you want to Deleting this ?");
+    let tf = confirm("Do you want to Deleting this ?");
         t=String(tf);
         if(t=="true"){document.getElementById("cdelete").setAttribute("value", "delete");}
     }
